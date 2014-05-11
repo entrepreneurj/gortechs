3b4628399069f7b2d05d4348fcd0b2dd516cb0a7
Joe <entrepreneurj@gmail.com>
2014-05-10 21:56:24 -0400

    Working websocket example. Whoop Whoop

--- gomultiwebsocket.go
+++ gomultiwebsocket.go
@@ -6,29 +6,18 @@ import (
 //	"io"
 //	"bytes"
 //	"log"
-	"bufio"
+//	"bufio"
 	"code.google.com/p/go.net/websocket"
 )
 
 // Echo the data received on the WebSocket
 func EchoServer(ws *websocket.Conn) {
-//	buf := new(bytes.Buffer)
-//	buf.ReadFrom(ws)
-	bufReader := bufio.NewReader(ws)
-	msg, err := bufReader.ReadString('\n')
+
+	var msg string
+	 websocket.Message.Receive(ws, &msg)
+	msg = "hiya"
 	fmt.Println(msg)
-	if err != nil {
-		panic(err)
-	}
-//	io.WriteString(ws, buf.String())
-//	io.Copy(ws, ws)
-//	fmt.Println(buf.String())
-//	if _, err := ws.Write([]byte(buf.String())); err != nil {
-//    		log.Fatal(err)
-//		fmt.Println("Error")
-//	}
-//	fmt.Println(buf.String())
-	ws.Write([]byte("hi"))
+	websocket.Message.Send(ws, msg)
 }
 
 func main() {

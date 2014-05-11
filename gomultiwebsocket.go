45d3569bc125d4e97e844867536089fc30a1d686
Joe <entrepreneurj@gmail.com>
2014-05-11 13:23:18 -0400

    roughly working! Uses a pool of redis connections and uses a connection for each rrequest

--- gomultiwebsocket.go
+++ gomultiwebsocket.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"log"
+	"time"
 //	"reflect"
 	"encoding/json"
 	"code.google.com/p/go.net/websocket"
@@ -12,18 +13,27 @@ import (
 )
 
 // Global var holding redis connection
-var redisConn redis.Conn
 
-// Initialises program. 
-// Opens connection to redis
-func init() {
+var (
+	pool *redis.Pool // Redis connection pool
+)
 
-	c, err := redis.Dial("tcp", ":6379")
-	if err!=nil {
-		fmt.Println(err)
-		panic(err)
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxActive: 0,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", ":6379")
+		       	if err!=nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+           		 _, err := c.Do("PING")
+            		return err
+        	},
 	}
-	redisConn=c
 }
 
 // Message structure for websockets.
@@ -56,6 +66,8 @@ func EchoServer(ws *websocket.Conn) {
 
 // Handles requests from clients wishing to subscribe to channels
 func SubServer(ws *websocket.Conn) {
+	redisConn := pool.Get()
+	defer redisConn.Close()
 
 	var msg T
 	websocket.JSON.Receive(ws, &msg)
@@ -63,16 +75,15 @@ func SubServer(ws *websocket.Conn) {
 	if msg.Type == "connect" {
 		fmt.Println("connect") // future implementation: possible authorisation for private channels
 	} else if msg.Type == "subscribe"  {
-		var channelExists bool = true //DoesChannelExist(msg.Channel)
+		var channelExists bool = true //DoesChannelExist(redisConn, msg.Channel)
 		fmt.Println("channel exist check", channelExists)
 		if channelExists == true {
-			chann := make(chan string)
+			chann := make(chan string,5)
 			psc := redis.PubSubConn{Conn: redisConn}
 			psc.Subscribe(msg.Channel)
-			defer psc.Unsubscribe()
 			go SubscriberListener(psc, chann)
-			SubscriberReporter(ws, chann)
-			defer fmt.Println("end of the line bud")	
+			SubscriberReporter(ws, psc, chann)
+			return	
 		} // else need to let client know to disconnect
 	} else {
 		msg.Type = "error"
@@ -85,14 +96,16 @@ func SubServer(ws *websocket.Conn) {
 }
 
 // Checks to see if event is publishing
-func DoesChannelExist(chanName string) (r bool) {
-	x, err := redisConn.Do("SISMEMBER", "pubsub.channels", chanName)
+func DoesChannelExist(conn redis.Conn, chanName string) (r bool) {
+	x, err := conn.Do("SISMEMBER", "pubsub.channels", chanName)
 	if err!= nil {
 		panic(err)
 	}
 	r = x != int64(0) // Turns int -> bool (i.e. is x not equal to 0?)
 	return r 
 }
+
+
 
 // Listens for messages from the channel
 func SubscriberListener(conn redis.PubSubConn, chann chan string)  {
@@ -104,9 +117,8 @@ func SubscriberListener(conn redis.PubSubConn, chann chan string)  {
 			case redis.Subscription:
 				 fmt.Printf("Subscription: %s %s %d\n", n.Kind, n.Channel, n.Count)
             			if n.Count == 0 {
-					conn.Unsubscribe()
+					fmt.Println(conn.Unsubscribe())
 					fmt.Println("Unsubscribed")
-                			return
             			}
 			case error:
 				fmt.Printf("error: %v\n", n)
@@ -117,17 +129,18 @@ func SubscriberListener(conn redis.PubSubConn, chann chan string)  {
 }
 
 // Collects messages from the channel and writes them to the wsocket
-func SubscriberReporter(ws *websocket.Conn, chann chan string) {
+func SubscriberReporter(ws *websocket.Conn, conn redis.PubSubConn, chann chan string) {
 	for {
 	
 	msgData := <- chann
 	if msgData == "unsubscribe" {
 		fmt.Println("Client to unsubscribe")
+		fmt.Println(conn.Unsubscribe())
 		break
 	} else {
 		msgDataJSON := &T{}
 		json.Unmarshal([]byte(msgData), &msgDataJSON)
-		fmt.Println(msgDataJSON.Type)
+		fmt.Println(msgDataJSON)
 		websocket.JSON.Send(ws, msgDataJSON)
 	}
 	}
@@ -136,12 +149,12 @@ func SubscriberReporter(ws *websocket.Conn, chann chan string) {
 
 
 func main() {
+	pool = newPool()
 	http.Handle("/echo", websocket.Handler(EchoServer))
 	http.Handle("/websockets/ws", websocket.Handler(SubServer))
 	err := http.ListenAndServe(":9009", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: " + "fatal" )
 	}
- 	defer redisConn.Close()
 }
 
